56e867a5e06cba06270df31760a50e28c9f85c62
hassyyy <nmohamedasan@gmail.com>
2022-04-25 22:48:07 +0530

    DRY: assign_amount

--- app/controllers/avo/sales_controller.rb
+++ app/controllers/avo/sales_controller.rb
@@ -1,7 +1,7 @@
 class Avo::SalesController < Avo::ResourcesController
   def create
     @sale = Sale.new(sale_params)
-    @sale.amount = @sale.quantity * (Cost.where(product_id: @sale.product_id, vendor_id: @sale.vendor_id).first&.amount).to_i
+    assign_amount
     @sale.save
 
     redirect_to "/avo/resources/sales"
@@ -10,7 +10,7 @@ class Avo::SalesController < Avo::ResourcesController
   def update
     @sale = Sale.find(params[:id])
     @sale.assign_attributes(sale_params)
-    @sale.amount = @sale.quantity * (Cost.where(product_id: @sale.product_id, vendor_id: @sale.vendor_id).first&.amount).to_i
+    assign_amount
     @sale.save
 
     redirect_to "/avo/resources/sales"
@@ -21,4 +21,9 @@ class Avo::SalesController < Avo::ResourcesController
   def sale_params
     params.require(:sale).permit(:date, :product_id, :vendor_id, :quantity)
   end
+
+  def assign_amount
+    @sale.amount = @sale.quantity * (Cost.where(product_id: @sale.product_id, vendor_id: @sale.vendor_id).first&.amount).to_i
+  end
+
 end
