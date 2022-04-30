c6976076df4bdc679b04d2c3a89ac11442fc0341
hassyyy <nmohamedasan@gmail.com>
2022-05-01 00:53:11 +0530

    Show errors on sale create/edit

--- app/controllers/avo/sales_controller.rb
+++ app/controllers/avo/sales_controller.rb
@@ -1,38 +1,27 @@
 class Avo::SalesController < Avo::ResourcesController
   def create
-    @sale = Sale.new(sale_params)
     assign_amount
-    @sale.save
-
-    redirect_to "/avo/resources/sales"
+    super
   end
 
   def update
-    @sale = Sale.find(params[:id])
-    @sale.assign_attributes(sale_params)
     assign_amount
-    @sale.save
-
-    redirect_to "/avo/resources/sales"
+    super
   end
 
   private
 
-  def sale_params
-    params.require(:sale).permit(:date, :product_id, :vendor_id, :quantity)
-  end
-
   def assign_amount
-    return if @sale.quantity.nil? || @sale.product_id.nil? || @sale.vendor_id.nil?
-    cost = Cost.where(product_id: @sale.product_id, vendor_id: @sale.vendor_id).first
+    return if @model.quantity.nil? || @model.product_id.nil? || @model.vendor_id.nil?
+    cost = Cost.where(product_id: @model.product_id, vendor_id: @model.vendor_id).first
     if cost.nil?
-      product = Product.where(id: @sale.product_id).first
+      product = Product.where(id: @model.product_id).first
       return if product.nil?
       price = product.amount
     else
       price = cost.amount
     end
-    @sale.amount = @sale.quantity * price
+    @model.amount = @model.quantity * price
   end
 
 end
