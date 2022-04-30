237b7c44d1aa537a5fd8e8b870e79cfc4cad63b7
hassyyy <nmohamedasan@gmail.com>
2022-05-01 01:04:18 +0530

    Add validation for cost for product & vendor

--- app/models/cost.rb
+++ app/models/cost.rb
@@ -6,4 +6,5 @@ class Cost < ApplicationRecord
   validates :product_id, presence: true
   validates :vendor_id, presence: true
   validates :amount, presence: true
+  validates_uniqueness_of :product_id, scope: :vendor_id, message: 'already has a cost for the selected Vendor'
 end
