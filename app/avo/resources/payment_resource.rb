e9e8bbb45893436914dfb5a89478b4df9f142013
hassyyy <nmohamedasan@gmail.com>
2022-06-27 21:50:42 +0530

    Use avo main branch

--- app/avo/resources/payment_resource.rb
+++ app/avo/resources/payment_resource.rb
@@ -5,7 +5,7 @@ class PaymentResource < Avo::BaseResource
   self.after_create_path = :index
   self.after_update_path = :index
 
-  field :date, as: :date, required: true, picker_format: 'd F, Y', format: '%d %B, %Y', default: Date.today
+  field :date, as: :date, required: true, picker_format: 'd F, Y', disable_mobile: true, format: '%d %B, %Y', default: Date.today
   field :vendor, as: :belongs_to, required: true
   field :amount, as: :number, required: true, format_using: -> (value) { "₹#{value}" }
 
